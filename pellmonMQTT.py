3380ec74e260a70b5be872ec509bc7b2e8f14775
Bjarne Christensen <github@edske.eu>
2024-12-27 19:34:03 +0100

    Cleanup of unused code

--- pellmonMQTT.py
+++ pellmonMQTT.py
@@ -15,7 +15,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from builtins import object
-import os
 import sys
 import argparse
 from time import sleep
@@ -23,18 +22,13 @@ from gi.repository import Gio, GLib
 import paho.mqtt.client as mosquitto
 import simplejson
 
-DEBUG = False
-
 class DbusNotConnected(Exception):
     """ This exception is raised when Dbus is not connected. """
-    print("DbusNotConnected: " + str(Exception))
-    #pass
+    print(f'DbusNotConnected: {str(Exception)}')
 
 class Dbus_handler(object):
     """ The Dbus_handler """
     def __init__(self, mq, bus, mqttTopic):
-        if DEBUG:
-            print(">>>>> dbus_init")
         if bus == 'SYSTEM':
             self.bustype = Gio.BusType.SYSTEM
         else:
@@ -44,8 +38,6 @@ class Dbus_handler(object):
 
     def start(self):
         """ start """
-        if DEBUG:
-            print(">>>>> dbus_start")
         self.notify = None
         self.bus = Gio.bus_get_sync(self.bustype, None)
         Gio.bus_watch_name(
@@ -58,10 +50,6 @@ class Dbus_handler(object):
 
     def dbus_connect(self, connection, name, owner):
         """ dbus_connect """
-
-        if DEBUG:
-            print(">>>>> dbus_connect")
-
         self.notify = Gio.DBusProxy.new_sync(
             self.bus,
             Gio.DBusProxyFlags.NONE,
@@ -80,19 +68,19 @@ class Dbus_handler(object):
                 print(f"Publish {str(value)} to {self.mqttTopic}/ {item['name']}")
                 self.mq.publish(f"{self.mqttTopic}/{item['name']}", value, qos=2, retain=True)
             except RuntimeError as error:
-                print("Exeption caught in Publish - dbus_connect: " + error)
-                pass
+                print(f'Exeption caught in Publish - dbus_connect: {error}')
 
     def subscribe(self):
         """Listen to the DBUS 'item changed' signal and publish changes at pellmon/_item_ """
-        if DEBUG:
-            print(">>>>> subscribe")
         def on_signal(proxy, sender_name, signal_name, parameters):
             parameter = parameters[0]
             msg = []
             msg = simplejson.loads(parameter)
             for dbusmsg in msg:
-                self.mq.publish(f"{self.mqttTopic}/{dbusmsg['name']}", dbusmsg['value'], qos=2, retain=True)
+                self.mq.publish(f"{self.mqttTopic}/{dbusmsg['name']}",
+                                dbusmsg['value'],
+                                qos=2,
+                                retain=True)
                 print(f"Publish {dbusmsg['value']} to {self.mqttTopic}/{dbusmsg['name']}")
         #Subscribe to all data items tagged with 'Settings' at pellmon/settings/_item
         self.settings = self.notify.GetFullDB('(as)', ['All',])
@@ -106,8 +94,6 @@ class Dbus_handler(object):
 
     def dbus_disconnect(self, connection, name):
         """ dbus_disconnect """
-        if DEBUG:
-            print(">>>>> dbus_disconnect")
         Status.dbus_connected = False
         Status.subscribed = False
         if self.notify:
@@ -115,8 +101,6 @@ class Dbus_handler(object):
 
     def getItem(self, itm):
         """ getItem """
-        if DEBUG:
-            print(">>>>> getItem")
         if self.notify:
             try:
                 return self.notify.GetItem('(s)', str(itm))
@@ -127,32 +111,24 @@ class Dbus_handler(object):
 
     def setItem(self, item, value):
         """ setItem """
-        if DEBUG:
-            print(">>>>> setItem")
         if self.notify:
             return self.notify.SetItem('(ss)',item, str(value))
         raise DbusNotConnected("server not running")
 
     def getdb(self):
         """ getdb """
-        if DEBUG:
-            print(">>>>> getdb")
         if self.notify:
             return self.notify.GetDB()
         raise DbusNotConnected("server not running")
 
     def getDBwithTags(self, tags):
         """ getDBwithTags """
-        if DEBUG:
-            print(">>>>> getDBwithTags")
         if self.notify:
             return self.notify.GetDBwithTags('(as)', tags)
         raise DbusNotConnected("server not running")
 
     def getFullDB(self, tags):
         """ getFullDB """
-        if DEBUG:
-            print(">>>>> getFullDB")
         if self.notify:
             db = self.notify.GetFullDB('(as)', str(tags))
             return db
@@ -160,26 +136,17 @@ class Dbus_handler(object):
 
     def getMenutags(self):
         """ getMenutags """
-        if DEBUG:
-            print(">>>>> getMenutags")
         if self.notify:
             return self.notify.getMenutags()
         raise DbusNotConnected("server not running")
 
-class Status(object):
+class Status:
     """ Class Status """
-    if DEBUG:
-        print(">>>>> Status")
     mqtt_connected = False
     dbus_connected = False
     subscribed = False
 
-###################
-#     Main entry  #
-###################
-if __name__ == "__main__":
-
-    """ Functions to be used by mqtt """
+if __name__ == '__main__':
     def on_connect(*args):
         """ on_connect """
         print("broker connected")
@@ -189,12 +156,10 @@ if __name__ == "__main__":
     def on_publish(*args):
         """ What do when calling on_publish """
         pass #print 'published'
-        #print('Publishing')
 
     def on_subscribe(*args):
         """ on_subscribe """
         pass #print 'subscribed'
-        #print('Subscribing')
 
     def on_disconnect(*args):
         """ on_disconnect """
@@ -209,11 +174,9 @@ if __name__ == "__main__":
         item = msg.topic.split('/')[-1]
         try:
             dbus.setItem(item, msg.payload)
-            #print('Set %s=%s, %s'%(item, msg.payload, dbus.setItem(item, msg.payload.decode("utf-8"))))
             print(f"Set {item}={msg.payload}, {dbus.setItem(item, msg.payload.decode('utf-8'))}" )
         except RuntimeError as error:
-            print("Exception caught: " + error)
-            pass
+            print(f'Exception caught: {error}')
 
     def manager():
         """ Manager binding dbus and mqtt together """
@@ -240,7 +203,8 @@ if __name__ == "__main__":
                         default='pellmon',
                         help='Defines the topic to publish/listen to, default is pellmon')
     parser.add_argument('-u', '--username',
-                        default='', help='Define a username which will be used to connect to the mqtt broker')
+                        default='',
+                        help='Define a username which will be used to connect to the mqtt broker')
     parser.add_argument('-P', '--password',
                         default='',
                         help='Define a password which will be used to connect to the mqtt broker')
@@ -282,10 +246,10 @@ if __name__ == "__main__":
             sleep(5)
 
     print("MQTT broker Connected..")
-    print("Python version: " + str(sys.version))
+    print(f'Python version: {str(sys.version)}')
     mqttc.loop_start()
 
-    print("Connected to broker ", arguments.host)
+    print(f'Connected to broker {arguments.host}')
     try:
         main_loop.run()
     except KeyboardInterrupt:
